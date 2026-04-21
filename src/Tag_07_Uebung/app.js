da0a673fb1532a72b574b0d1e616691d7cb03a45
AlphaTrainee <alpha@f-walter.de>
2026-04-21 15:54:56 +0200

    Übung Tag 7 fertiggestellt

---
+++ src/Tag_07_Uebung/app.js
@@ -0,0 +1,27 @@
+
+/* 
+
+// Defaults für init:
+npm config set init-license "MIT"
+npm config set init-type "module"
+
+npm init -y
+npm i https://raw.githubusercontent.com/AlphaTrainee/NodeJS/refs/heads/main/modules/01_random_numbers/01_random_numbers-1.0.0.tgz
+
+*/
+
+// Wir importieren die Funktion unter dem Namen, den du in der 
+// package.json als Key für die Dependency vergeben hast (lotto-tool)
+import { generateRandomNumbers } from '01_random_numbers';
+
+console.log("--- MEIN LOTTO-GENERATOR ---");
+
+try {
+    const luckyNumbers = generateRandomNumbers();
+    
+    console.log("Die heutigen Gewinnzahlen sind:");
+    console.log(luckyNumbers.join(' - '));
+    console.log(" Viel Glück! 🍀");
+} catch (error) {
+    console.error("Fehler beim Generieren der Zahlen:", error.message);
+}
